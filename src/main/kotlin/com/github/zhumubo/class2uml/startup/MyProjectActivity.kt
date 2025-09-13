54416e25d5cd4bb19e156bb16d3ddb2b715036ef
GitHub Action <action@github.com>
2025-09-13 14:34:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/zhumubo/class2uml/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/zhumubo/class2uml/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.zhumubo.class2uml.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
